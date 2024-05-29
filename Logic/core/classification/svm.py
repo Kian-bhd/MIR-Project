a9ea67055afeaf8b772647dfd882ee2f89b27f25
Kian Bahadori <kkibian@gmail.com>
2024-05-29 15:53:40 +0330

    Added SVM

--- Logic/core/classification/svm.py
+++ Logic/core/classification/svm.py
@@ -7,9 +7,9 @@ from .data_loader import ReviewLoader
 
 
 class SVMClassifier(BasicClassifier):
-    def __init__(self):
+    def __init__(self, C=5):
         super().__init__()
-        self.model = SVC()
+        self.model = SVC(C=C)
 
     def fit(self, x, y):
         """
@@ -21,7 +21,7 @@ class SVMClassifier(BasicClassifier):
         y: np.ndarray
             The real class label for each doc
         """
-        pass
+        self.model.fit(x, y)
 
     def predict(self, x):
         """
@@ -35,7 +35,7 @@ class SVMClassifier(BasicClassifier):
             Return the predicted class for each doc
             with the highest probability (argmax)
         """
-        pass
+        return self.model.predict(x)
 
     def prediction_report(self, x, y):
         """
@@ -50,12 +50,19 @@ class SVMClassifier(BasicClassifier):
         str
             Return the classification report
         """
-        pass
-
+        y_pred = self.predict(x)
+        return classification_report(y, y_pred)
 
 # F1 accuracy : 78%
 if __name__ == '__main__':
     """
     Fit the model with the training data and predict the test data, then print the classification report
     """
-    pass
+    review_loader = ReviewLoader(file_path='../../IMDB Dataset.csv')
+    review_loader.load_data()
+    review_loader.get_embeddings()
+    x_train, x_test, y_train, y_test = review_loader.split_data(test_data_ratio=0.2)
+    svm_classifier = SVMClassifier(C=5)
+    svm_classifier.fit(x_train, y_train)
+    classification_report = svm_classifier.prediction_report(x_test, y_test)
+    print(classification_report)
